72a4f43c51ec8c649b31d167216c63489e0869dc
dongeun-i <dongeun1014@naver.com>
2023-01-04 23:31:59 +0900

    add Suspense

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 
@@ -6,7 +6,11 @@ import { PageList } from "./pages/page_list";
 
 const App: React.FunctionComponent<any> = () => {
     const [isLogin, setisLogin] = useState<boolean>(false);
-    return <PageList isLogin={isLogin} />;
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <PageList isLogin={isLogin} />;
+        </Suspense>
+    );
 };
 
 export default App;
